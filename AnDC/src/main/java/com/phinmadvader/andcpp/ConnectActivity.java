b35f5bd7e5f270cc6c760fa7fe10ca8082be42dd
invader <sanjay.slnarayanan@gmail.com>
2013-08-13 22:17:08 +0530

    Added connect activity changes

--- AnDC/src/main/java/com/phinmadvader/andcpp/ConnectActivity.java
+++ AnDC/src/main/java/com/phinmadvader/andcpp/ConnectActivity.java
@@ -1,24 +1,85 @@
 package com.phinmadvader.andcpp;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.IBinder;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
-public class ConnectActivity extends Activity {
+public class ConnectActivity extends FragmentActivity {
+    // Declare Variables
+    public ViewPager viewPager;
+    public PagerAdapter adapter;
+    public Intent serviceIntent;
+    public DCPPService mService;
+    SharedPreferences settings;
+    boolean mBound = false;
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            DCPPService.LocalBinder binder = (DCPPService.LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBound = false;
+            finish();
+        }
+    };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Get the view from viewpager_main.xml
         setContentView(R.layout.connect_activity);
-        startActivity(new android.content.Intent(this, TestActivity.class));
+
+        settings = getSharedPreferences(Constants.PREFS_NAME, 0);
+
+        // Locate the ViewPager in viewpager_main.xml
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        // Pass results to ViewPagerAdapter Class
+        adapter = new ViewPageAdapter(this);
+        // Binds the Adapter to the ViewPager
+        viewPager.setAdapter(adapter);
     }
 
-
+    // Not using options menu in this tutorial
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.connect, menu);
         return true;
     }
-    
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mBound) {
+            unbindService(mConnection);
+            mBound = false;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        bindService(new Intent(this, DCPPService.class), mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    public void moveToPage(int page) {
+        viewPager.setCurrentItem(page);
+    }
 }
--- AnDC/src/main/java/com/phinmadvader/andcpp/UserListView.java
+++ AnDC/src/main/java/com/phinmadvader/andcpp/UserListView.java
@@ -1,7 +1,38 @@
 package com.phinmadvader.andcpp;
 
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
 /**
  * Created by invader on 8/13/13.
  */
-public class UserListView {
+public class UserListView extends LinearLayout{
+    ConnectActivity connectActivity;
+    View view;
+    ViewPageAdapter pageAdapter;
+    ListView userListView;
+    TextView connectedUserCount;
+
+
+    public UserListView( ConnectActivity connectActivity, ViewPageAdapter pageAdapter) {
+        super(connectActivity);
+        this.connectActivity = connectActivity;
+        this.pageAdapter = pageAdapter;
+        LayoutInflater mInflater = (LayoutInflater)connectActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mInflater.inflate(R.layout.userlist_layout, this, true);
+        userListView = (ListView) findViewById(R.id.listView);
+        userListView.setAdapter(pageAdapter.userListAdapter);
+        connectedUserCount = (TextView) findViewById(R.id.textView2);
+    }
+
+    public void setConnectedUserCount(int count) {
+        connectedUserCount.setText(String.valueOf(count));
+    }
 }
